ceba7d9480f94e827f61acbefe31c5d7cba27b3d
Georgiy Goloborodov <barstyle@mail.ru>
2022-04-12 19:11:06 +0300

    add gitignore

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    // Реализуй класс Main с методом public static String calc(String input).
+    public String calc(String input) {
+
+        return input;
+
+    }
+
+    public void main(String[] args) {
+        System.out.println(calc( "1 + 1" ));
+        System.out.println(calc( "1+1" ));
+
+    }
+}
