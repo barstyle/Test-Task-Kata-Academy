d82a9e8d62b43aecf535ae4b8b34e5098b6f6909
Georgiy Goloborodov <barstyle@mail.ru>
2022-04-13 12:23:59 +0300

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,58 @@
-import java.util.Scanner;
+import java.util.*;
 
+// Реализуй класс Main с методом public static String calc(String input).
 public class Main {
-    // Реализуй класс Main с методом public static String calc(String input).
-    public String calc(String input) {
 
-        return input;
+    public static String calc(String input) {
+
+        String[] operators = {"+", "-", "/", "*"};
+
+        Boolean isArabic = true;
+
+        HashMap<String, Integer> tupleRomano = new HashMap<>();
+        tupleRomano.put("I", 1);
+        tupleRomano.put("II", 2);
+        tupleRomano.put("III", 3);
+        tupleRomano.put("IV", 4);
+        tupleRomano.put("V", 5);
+        tupleRomano.put("VI", 6);
+        tupleRomano.put("VII", 7);
+        tupleRomano.put("VIII", 8);
+        tupleRomano.put("IX", 9);
+        tupleRomano.put("X", 10);
+
+
+
+
+        List<String> operands = new ArrayList<>();
+
+
+        for (int i = 0; i < input.split(" ").length; i++) {
+
+            operands.add(input.split(" ")[i]);
+        }
+
+        System.out.println(Arrays.toString(operands.toArray()));
+
+        int result;
+
+        switch (operands.get(1)) {
+            case "+":
+                result = Integer.parseInt(operands.get(0)) + Integer.parseInt(operands.get(2));
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + operands.get(1));
+        }
+
+        return String.valueOf(result);
 
     }
 
-    public void main(String[] args) {
-        System.out.println(calc( "1 + 1" ));
-        System.out.println(calc( "1+1" ));
+    public static void main(String[] args) {
+        System.out.println(calc("1 + 1"));
+        System.out.println(calc("10 + 10"));
+        System.out.println(calc("10 % 10"));
+
 
     }
 }
