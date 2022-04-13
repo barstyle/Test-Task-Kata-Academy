07eebb425ab388e57461b8881ad58275ebc6a518
Georgiy Goloborodov <barstyle@mail.ru>
2022-04-13 20:31:56 +0300

    intermediate stage

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.*;
 
 // Реализуй класс Main с методом public static String calc(String input).
@@ -7,9 +8,10 @@ public class Main {
 
         String[] operators = {"+", "-", "/", "*"};
 
-        Boolean isArabic = true;
+        Boolean isArabic;
+        Boolean isRomano;
 
-        HashMap<String, Integer> tupleRomano = new HashMap<>();
+        HashMap<String, Integer> tupleRomano = new HashMap<String, Integer>();
         tupleRomano.put("I", 1);
         tupleRomano.put("II", 2);
         tupleRomano.put("III", 3);
@@ -21,28 +23,25 @@ public class Main {
         tupleRomano.put("IX", 9);
         tupleRomano.put("X", 10);
 
+        ArrayList<String> expression = new ArrayList<>(Arrays.asList(input.strip().split(" ")));
 
+        System.out.println(Arrays.toString(expression.toArray()));
 
-
-        List<String> operands = new ArrayList<>();
-
-
-        for (int i = 0; i < input.split(" ").length; i++) {
-
-            operands.add(input.split(" ")[i]);
+        if (expression.size() != 3) {
+            try {
+                throw new IOException("ОШИБКА ВВОДА-ВЫВОДА!\nт.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            } catch (IOException e) {
+                return e.getMessage();
+            }
         }
 
-        System.out.println(Arrays.toString(operands.toArray()));
-
-        int result;
-
-        switch (operands.get(1)) {
-            case "+":
-                result = Integer.parseInt(operands.get(0)) + Integer.parseInt(operands.get(2));
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + operands.get(1));
-        }
+        int result = switch (expression.get(1)) {
+            case "+" -> Integer.parseInt(expression.get(0)) + Integer.parseInt(expression.get(2));
+            case "-" -> Integer.parseInt(expression.get(0)) - Integer.parseInt(expression.get(2));
+            case "*" -> Integer.parseInt(expression.get(0)) * Integer.parseInt(expression.get(2));
+            case "/" -> Integer.parseInt(expression.get(0)) / Integer.parseInt(expression.get(2));
+            default -> throw new IllegalStateException("Unexpected value: " + expression.get(1));
+        };
 
         return String.valueOf(result);
 
@@ -51,8 +50,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(calc("1 + 1"));
         System.out.println(calc("10 + 10"));
-        System.out.println(calc("10 % 10"));
-
+        System.out.println(calc("10 / 5"));
+        System.out.println(calc("5 / 2"));
+        System.out.println(calc("5 + 2 +"));
+        System.out.println(calc(" 5 + 2 "));
+        System.out.println(calc(" 5 / 0 "));
 
     }
 }
