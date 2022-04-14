bd347cafa4ee08684e9997fe09fe9e5c3de163e2
Georgiy Goloborodov <barstyle@mail.ru>
2022-04-14 13:14:25 +0300

    intermediate stage

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,17 @@ public class Main {
         Boolean isArabic;
         Boolean isRomano;
 
-        HashMap<String, Integer> tupleRomano = new HashMap<String, Integer>();
-        tupleRomano.put("I", 1);
-        tupleRomano.put("II", 2);
-        tupleRomano.put("III", 3);
-        tupleRomano.put("IV", 4);
-        tupleRomano.put("V", 5);
-        tupleRomano.put("VI", 6);
-        tupleRomano.put("VII", 7);
-        tupleRomano.put("VIII", 8);
-        tupleRomano.put("IX", 9);
-        tupleRomano.put("X", 10);
+        HashMap<String, String> tupleRomano = new HashMap<String, String>();
+        tupleRomano.put("I", "1");
+        tupleRomano.put("II", "2");
+        tupleRomano.put("III", "3");
+        tupleRomano.put("IV", "4");
+        tupleRomano.put("V", "5");
+        tupleRomano.put("VI", "6");
+        tupleRomano.put("VII", "7");
+        tupleRomano.put("VIII", "8");
+        tupleRomano.put("IX", "9");
+        tupleRomano.put("X", "10");
 
         ArrayList<String> expression = new ArrayList<>(Arrays.asList(input.strip().split(" ")));
 
@@ -37,6 +37,30 @@ public class Main {
                 return e.getMessage();
             }
         }
+
+        isArabic = tupleRomano.containsValue(expression.get(0)) | tupleRomano.containsValue(expression.get(2));
+        isRomano = tupleRomano.containsKey(expression.get(0)) | tupleRomano.containsKey(expression.get(2));
+
+        System.out.println(isArabic);
+        System.out.println(isRomano);
+
+        if (isArabic == isRomano) {
+            try {
+                throw new IOException("ОШИБКА ВВОДА-ВЫВОДА!\n" +
+                        "т.к. формат математической операции не удовлетворяет заданию " +
+                        "- Либо only РИМСКИЕ - Либо only АРАБСКИЕ, камон!?");
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        if (isRomano) {
+            expression.set(0, tupleRomano.get(expression.get(0)));
+            expression.set(2, tupleRomano.get(expression.get(2)));
+        }
+
+        System.out.println(expression.toString());
+
         try {
             result = switch (expression.get(1)) {
                 case "+" -> Integer.parseInt(expression.get(0)) + Integer.parseInt(expression.get(2));
@@ -54,7 +78,25 @@ public class Main {
         } catch (IllegalStateException e) {
             return e.getMessage();
         }
-        return String.valueOf(result);
+        if (isRomano) {
+            tupleRomano.put("XI", "11");
+            tupleRomano.put("XII", "12");
+            tupleRomano.put("XIII", "13");
+            tupleRomano.put("XIV", "14");
+            tupleRomano.put("XV", "15");
+            tupleRomano.put("XVI", "16");
+            tupleRomano.put("XVII", "17");
+            tupleRomano.put("XVIII", "18");
+            tupleRomano.put("XIX", "19");
+            tupleRomano.put("XX", "20");
+
+            for (String res: tupleRomano.keySet()) {
+                if(tupleRomano.get(res).equals(String.valueOf(result))) {
+                    System.out.println("!".repeat(50));
+                    return tupleRomano.get(res);
+                }
+            }
+        } return String.valueOf(result);
 
     }
 
@@ -67,6 +109,9 @@ public class Main {
         System.out.println(calc(" 5 + 2 "));
         System.out.println(calc(" 5 / + "));
         System.out.println(calc(" 5 % 2 "));
+        System.out.println(calc(" 5+2 "));
+        System.out.println(calc("1 + III"));
+        System.out.println(calc("V + III"));
 
     }
 }
