dfeeb327e794972b6686b43823e604b77f92fd11
Georgiy Goloborodov <barstyle@mail.ru>
2022-04-15 14:22:08 +0300

    intermediate stage

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,42 @@ public class Main {
 
     public static String calc(String input) {
 
+        System.out.println("Expression: " + input);
+
         String[] operators = {"+", "-", "/", "*"};
+        String operator = " ";
+        int countOperators = 0;
+
+        for (String o: operators) {
+            if (input.contains(o)) {
+                operator = o;
+                countOperators++;
+                System.out.println("countOperators - " + countOperators);
+            }
+        }
+
+        if (countOperators != 1) {
+            try {
+                throw new IOException("Оператор должен быть один и только: (+, -, /, *)");
+        } catch (IOException e) {
+                return e.getMessage();
+            }
+
+        }
+
         int result;
+
+        ArrayList<String> expression;
+        expression = new ArrayList<>(Arrays.asList(input
+                .replaceAll(" ", "")
+                .split("\\" + operator)));
+
+        System.out.println(Arrays.toString(expression.toArray()));
 
         Boolean isArabic;
         Boolean isRomano;
 
-        HashMap<String, String> tupleRomano = new HashMap<String, String>();
+        HashMap<String, String> tupleRomano = new HashMap<>();
         tupleRomano.put("I", "1");
         tupleRomano.put("II", "2");
         tupleRomano.put("III", "3");
@@ -24,11 +53,9 @@ public class Main {
         tupleRomano.put("IX", "9");
         tupleRomano.put("X", "10");
 
-        ArrayList<String> expression = new ArrayList<>(Arrays.asList(input.strip().split(" ")));
 
-        System.out.println(Arrays.toString(expression.toArray()));
 
-        if (expression.size() != 3) {
+        if (expression.size() != 2) {
             try {
                 throw new IOException("ОШИБКА ВВОДА-ВЫВОДА!\n" +
                         "т.к. формат математической операции не удовлетворяет заданию " +
@@ -38,8 +65,8 @@ public class Main {
             }
         }
 
-        isArabic = tupleRomano.containsValue(expression.get(0)) | tupleRomano.containsValue(expression.get(2));
-        isRomano = tupleRomano.containsKey(expression.get(0)) | tupleRomano.containsKey(expression.get(2));
+        isArabic = tupleRomano.containsValue(expression.get(0)) | tupleRomano.containsValue(expression.get(1));
+        isRomano = tupleRomano.containsKey(expression.get(1)) | tupleRomano.containsKey(expression.get(1));
 
         System.out.println(isArabic);
         System.out.println(isRomano);
@@ -56,17 +83,15 @@ public class Main {
 
         if (isRomano) {
             expression.set(0, tupleRomano.get(expression.get(0)));
-            expression.set(2, tupleRomano.get(expression.get(2)));
+            expression.set(1, tupleRomano.get(expression.get(1)));
         }
 
-        System.out.println(expression.toString());
-
         try {
-            result = switch (expression.get(1)) {
-                case "+" -> Integer.parseInt(expression.get(0)) + Integer.parseInt(expression.get(2));
-                case "-" -> Integer.parseInt(expression.get(0)) - Integer.parseInt(expression.get(2));
-                case "*" -> Integer.parseInt(expression.get(0)) * Integer.parseInt(expression.get(2));
-                case "/" -> Integer.parseInt(expression.get(0)) / Integer.parseInt(expression.get(2));
+            result = switch (operator) {
+                case "+" -> Integer.parseInt(expression.get(0)) + Integer.parseInt(expression.get(1));
+                case "-" -> Integer.parseInt(expression.get(0)) - Integer.parseInt(expression.get(1));
+                case "*" -> Integer.parseInt(expression.get(0)) * Integer.parseInt(expression.get(1));
+                case "/" -> Integer.parseInt(expression.get(0)) / Integer.parseInt(expression.get(1));
                 default -> throw new IllegalStateException("ОШИБКА ВВОДА-ВЫВОДА!\n" +
                         "Калькулятор умеет выполнять только операции сложения, вычитания, умножения и деления\n" +
                         "и не поддерживает оператор: "
@@ -78,7 +103,18 @@ public class Main {
         } catch (IllegalStateException e) {
             return e.getMessage();
         }
-        if (isRomano) {
+
+        if (Integer.parseInt(expression.get(0)) < 1 | Integer.parseInt(expression.get(0)) > 10
+                | Integer.parseInt(expression.get(1)) < 1 | Integer.parseInt(expression.get(1)) > 10) {
+            try {
+                throw new IOException("ОШИБКА ВВОДА-ВЫВОДА!\n" +
+                        "Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более");
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        if (isRomano & result > 1) {
             tupleRomano.put("XI", "11");
             tupleRomano.put("XII", "12");
             tupleRomano.put("XIII", "13");
@@ -90,14 +126,14 @@ public class Main {
             tupleRomano.put("XIX", "19");
             tupleRomano.put("XX", "20");
 
-            for (String res: tupleRomano.keySet()) {
-                if(tupleRomano.get(res).equals(String.valueOf(result))) {
-                    System.out.println("!".repeat(50));
-                    return tupleRomano.get(res);
+            for (String res : tupleRomano.keySet()) {
+                if (tupleRomano.get(res).equals(String.valueOf(result))) {
+                    return res;
                 }
             }
-        } return String.valueOf(result);
-
+        }
+        if (isArabic) return String.valueOf(result);
+        return "В Риме нет чисел меньше Единицы - только ВПЕРЕД! - только НАВЕРХ!";
     }
 
     public static void main(String[] args) {
@@ -112,6 +148,10 @@ public class Main {
         System.out.println(calc(" 5+2 "));
         System.out.println(calc("1 + III"));
         System.out.println(calc("V + III"));
-
+        System.out.println(calc("IX + III"));
+        System.out.println(calc("11 + 2"));
+        System.out.println(calc("1 - 2"));
+        System.out.println(calc("I - II"));
+        System.out.println(calc("III"));
     }
 }
